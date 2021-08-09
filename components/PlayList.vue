5d6ba12f8e4a18f44551c8d27e88be6c773f0c66
IrynaHolovatska <iryna.holovatska@globallogic.com>
2021-08-09 09:43:25 +0300

    init

--- components/PlayList.vue
+++ components/PlayList.vue
@@ -1,40 +1,148 @@
 <template>
-<section>
-  <div v-for="video in videos" :key="video.id">
-    <div class="thumbnail-img">
-      <div @click="chooseVideo(video)" class="thumbnail">
-        <img :src="video.cover"/>
-      </div>
-    </div>
-    <div class="thumbnail-info">
-      <h3>{{ video.title }}</h3>
-      <p>{{ video.subtitle }}</p>
-    </div>
-
-  </div>
-</section>
+      <section class="playlist">
+        <h3>Now Playing <span> 🎵 </span></h3>
+        <ul>
+          <li v-for="video in videos" :key="video.id" class="video-list">
+            <div class="cover-playlist" @click="chooseVideo(video)">
+              <div><img :src="video.cover" class="cover"/> </div>
+              <div class="details">
+                <h2 class="video-title">
+                  {{ video.title }}
+                </h2>
+                <p class="subtitle">{{ video.subtitle }}</p>
+              </div>
+            </div>
+            <div class="actions">
+              <button @click="removeVideo(video)" class="delete">
+                <img src="../static/img/images.png" alt="times">
+              </button>
+            </div>
+          </li>
+        </ul>
+      </section>
 </template>
 
 <script>
 export default {
   name: "PlayList",
-  props: ['videos', 'activeVideo'],
+  props: ['videos'],
   data: () => ({
-    videoF: this.activeVideo
   }),
   methods: {
     chooseVideo(video) {
-      //SET VIDEO AS ACTIVE VIDEO
-      this.videoF = video;
-      console.log(video)
-      // //INCREASE THE VIDEOS VIEWS BY 1
-      // video.views += 1;
-      console.log(this.videoF)
+      this.$emit('choose', video);
+
+    },
+    removeVideo(video) {
+      this.$emit('remove', video);
     }
   }
 }
 </script>
 
 <style scoped>
+.playlist {
+  max-width: 375px;
+  max-height: 623px;
+  margin: 0 auto;
+  margin: 0;
+  box-sizing: border-box;
+  background-color: #fff;
+  overflow-y: auto;
+  border-radius: 5px;
+}
+
+.playlist h3 {
+  color: #212121;
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+.playlist .video-list {
+  padding: 10px;
+}
+.cover-playlist {
+  display: flex;
+}
+
+.playlist .video-list {
+  display: flex;
+  padding: 10px;
+}
+
+.playlist .video-list:hover {
+  background-color: #ededed;
+  transition: box-shadow .2s,background-color .3s;
+  cursor: pointer;
+}
+.cover {
+  width: 50px;
+  height: 100%;
+  box-shadow: 0 24px 35px -16px rgb(107 179 237);
+  border-radius: 5px;
+}
+
+
+.details {
+  margin-left: 10px;
+  width: 200px;
+}
+.details > .video-title {
+  color: #585858;
+  font-size: inherit;
+  text-align: left;
+}
+
+.details > .subtitle {
+  color: #5d5555;
+  text-align: left;
+}
+
+.video-title {
+  margin-bottom: 5px;
+}
+.actions > .delete {
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  justify-content: center;
+  box-shadow: -1px 17px 24px -6px rgba(0,0,0,.2);
+  cursor: pointer;
+  font-size: 20px;
+  color: #ba83ca;
+}
+button img {
+  width: 12px;
+}
+button:hover {
+  opacity: 0.8;
+  transform: scale(1.1);
+}
+::-webkit-scrollbar-track
+{
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+  background-color: #F5F5F5;
+}
+
+::-webkit-scrollbar
+{
+  width: 10px;
+  background-color: #F5F5F5;
+}
+
+::-webkit-scrollbar-thumb
+{
+  background-color: #ba83ca;
+  background-image: -webkit-linear-gradient(45deg,
+  rgba(255, 255, 255, .2) 25%,
+  transparent 25%,
+  transparent 50%,
+  rgba(255, 255, 255, .2) 50%,
+  rgba(255, 255, 255, .2) 75%,
+  transparent 75%,
+  transparent)
+}
 
 </style>
---
+++ static/dataVideos/videos.js
@@ -0,0 +1,96 @@
+import video1 from "../videos/Big_Buck_Bunny_1080_10s_2MB.mp4";
+import video2 from "../videos/Jellyfish_720_10s_2MB.mp4";
+import video3 from "../videos/Sintel_1080_10s_1MB.mp4";
+import video4 from "../videos/VolkswagenGTIReview.mp4";
+import video5 from "../videos/ForBiggerFun.mp4";
+
+import img1 from "../img/big_buck_bunny (2).jpg";
+import img2 from "../img/jellyfish.jpg";
+import img3 from "../img/sintel.jpg";
+import img4 from "../img/garage.jpg";
+import img5 from "../img/maxresdefault.jpg"
+
+const videos = [
+    {
+        id: 1,
+        title: "Big Buck Bunny",
+        subtitle: "By Blender Foundation",
+        seconds: 10,
+        cover: img1,
+        src: video1
+    },
+    {
+        id: 2,
+        title: "Jellyfish",
+        subtitle: "Jellyfish video",
+        seconds: 10,
+        cover:img2,
+        src: video2
+    },
+    {
+        id: 3,
+        title: "Sintel",
+        subtitle: "By Sintel Foundation",
+        seconds: 10,
+        cover: img3,
+        src: video3
+    },
+    {
+        id: 4,
+        title: "Volkswagen GTI Review",
+        subtitle: "By Garage419",
+        seconds: 593,
+        cover: img4,
+        src: video4,
+    },
+    {
+        id: 5,
+        title: "We Are Going On Bullrun",
+        subtitle: "By Garage419",
+        seconds: 60,
+        cover: img5,
+        src: video5,
+    },
+  {
+    id: 1,
+    title: "Big Buck Bunny",
+    subtitle: "By Blender Foundation",
+    seconds: 10,
+    cover: img1,
+    src: video1
+  },
+  {
+    id: 2,
+    title: "Jellyfish",
+    subtitle: "Jellyfish video",
+    seconds: 10,
+    cover:img2,
+    src: video2,
+  },
+  {
+    id: 3,
+    title: "Sintel",
+    subtitle: "By Sintel Foundation",
+    seconds: 10,
+    cover: img3,
+    src: video3
+  },
+  {
+    id: 4,
+    title: "Volkswagen GTI Review",
+    subtitle: "By Garage419",
+    seconds: 593,
+    cover: img4,
+    src: video4,
+  },
+  {
+    id: 5,
+    title: "We Are Going On Bullrun",
+    subtitle: "By Garage419",
+    seconds: 60,
+    cover: img5,
+    src: video5,
+  }
+];
+
+export default videos;
